0c4a6fd027ff16473afe38eeb19c3d14409dbfef
muyol <muyol@sina.com>
2016-12-19 20:21:48 +0800

    init code
    init code

---
+++ data/config.php
@@ -0,0 +1,37 @@
+<?php
+// database host
+$db_host   = "localhost:3306";
+
+// database name
+$db_name   = "ximenzijiadian";
+
+// database username
+$db_user   = "ximenzijiadian";
+
+// database password
+$db_pass   = "bohity";
+
+// table prefix
+$prefix    = "ecs_";
+
+$timezone    = "Asia/Chongqing";
+
+$cookie_path    = "/";
+
+$cookie_domain    = "";
+
+$session = "1440";
+
+define('EC_CHARSET','utf-8');
+
+define('ADMIN_PATH','admin');
+
+define('AUTH_KEY', 'this is a key');
+
+define('OLD_AUTH_KEY', '');
+
+define('API_TIME', '2016-12-19 14:18:50');
+
+define('STORE_KEY','b74b264e4b020496fe3377e2426e06bd');
+
+?>
---
+++ data/flashdata/dynfocus/data.js
@@ -0,0 +1,27 @@
+imgUrl1="data/afficheimg/20160128exxdnz.jpg";
+imgtext1="";
+imgLink1=escape("http://mall.honrisen.com");
+
+imgUrl2="data/afficheimg/20160129prsrpo.jpg";
+imgtext2="";
+imgLink2=escape("http://mall.honrisen.com");
+
+imgUrl3="data/afficheimg/20160128exxdnz.jpg";
+imgtext3="";
+imgLink3=escape("http://mall.honrisen.com");
+
+imgUrl4="data/afficheimg/20160129prsrpo.jpg";
+imgtext4="";
+imgLink4=escape("http://mall.honrisen.com");
+
+imgUrl5="data/afficheimg/20160128exxdnz.jpg";
+imgtext5="";
+imgLink5=escape("http://mall.honrisen.com");
+
+imgUrl6="data/afficheimg/20160129prsrpo.jpg";
+imgtext6="";
+imgLink6=escape("http://mall.honrisen.com");
+
+var pics=imgUrl1+"|"+imgUrl2+"|"+imgUrl3+"|"+imgUrl4+"|"+imgUrl5+"|"+imgUrl6;
+var links=imgLink1+"|"+imgLink2+"|"+imgLink3+"|"+imgLink4+"|"+imgLink5+"|"+imgLink6;
+var texts=imgtext1+"|"+imgtext2+"|"+imgtext3+"|"+imgtext4+"|"+imgtext5+"|"+imgtext6;
