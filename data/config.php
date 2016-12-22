7dba1f134ecbcca8720402c6000cc50b7feb9a4a
muyol <muyol@sina.com>
2016-12-23 07:53:04 +0800

    个人中心
    修改收货地址、团购订单查询、秒杀订单查询、已购买商品查询、待付款商品

--- data/config.php
+++ data/config.php
@@ -15,7 +15,8 @@ $db_host   = "localhost:3306";
 // $db_pass   = "root";
 
 // database name
-$db_name   = "ecshop_three";
+// $db_name   = "ecshop_three";
+$db_name   = "esc";
 
 // database username
 $db_user   = "root";
@@ -43,7 +44,7 @@ define('AUTH_KEY', 'this is a key');
 
 define('OLD_AUTH_KEY', '');
 
-define('API_TIME', '2016-12-22 10:54:53');
+define('API_TIME', '2016-12-23 03:27:52');
 
 define('STORE_KEY','b74b264e4b020496fe3377e2426e06bd');
 
