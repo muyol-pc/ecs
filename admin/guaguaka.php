2453aad6e940bcd9e4c43767c4db4094bad7616a
zhongyuzhi <251280920@qq.com>
2016-12-26 00:04:19 +0800

    dazhuanpan

---
+++ admin/guaguaka.php
@@ -0,0 +1,14 @@
+<?php
+define('IN_ECS', true);
+
+require(dirname(__FILE__) . '/includes/init.php');
+
+/* act操作项的初始化 */
+if (empty($_REQUEST['act']))
+{
+    $_REQUEST['act'] = 'list';
+}
+else
+{
+    $_REQUEST['act'] = trim($_REQUEST['act']);
+}
