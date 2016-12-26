140938e8836ae0e2e2d1a1254e02a9cd144e022f
zhongyuzhi <251280920@qq.com>
2016-12-26 08:13:24 +0800

    merge

--- data/config.php
+++ data/config.php
@@ -3,15 +3,15 @@
 $db_host   = "localhost:3306";
 
 // database name
-// $db_name   = "ximenzijiadian";
-$db_name   = "ecshop2";
+ $db_name   = "ximenzijiadian";
+//$db_name   = "ecshop2";
 
 // database username
-// $db_user   = "ximenzijiadian";
+ $db_user   = "ximenzijiadian";
 // $db_user   = "root";
 
 // database password
-// $db_pass   = "bohity";
+ $db_pass   = "bohity";
 // $db_pass   = "root";
 
 // database name
@@ -44,11 +44,8 @@ define('AUTH_KEY', 'this is a key');
 
 define('OLD_AUTH_KEY', '');
 
-<<<<<<< HEAD
 define('API_TIME', '2016-12-26 02:40:01');
-=======
-define('API_TIME', '2016-12-26 01:37:33');
->>>>>>> b3de84c4cd4acb2d9022617f4a4260cb6499f1cb
+
 
 define('STORE_KEY','b74b264e4b020496fe3377e2426e06bd');
 
